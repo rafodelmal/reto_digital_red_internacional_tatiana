3c1486e131457a751dd192d82630440736d11ff9
Rafo <rafo.reloaded@gmail.com>
2021-03-08 10:00:44 -0500

    Estructura

---
+++ backend/src/main/java/com/example/hexagonal/architecture/domain/service/ProductServiceImplementation.java
@@ -0,0 +1,39 @@
+package com.example.hexagonal.architecture.domain.service;
+
+import com.example.hexagonal.architecture.domain.model.Product;
+import com.example.hexagonal.architecture.port.ProductRepository;
+import com.example.hexagonal.architecture.port.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * The class is an use case implementation of the inbound port.
+ */
+@Service
+public class ProductServiceImplementation implements ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Override
+    public List<Product> getProducts() {
+        return productRepository.getProducts();
+    }
+
+    @Override
+    public Product getProductById(Integer productId) {
+        return productRepository.getProductById(productId);
+    }
+
+    @Override
+    public Product addProduct(Product product) {
+        return productRepository.addProduct(product);
+    }
+
+    @Override
+    public Product removeProduct(Integer productId) {
+        return productRepository.removeProduct(productId);
+    }
+}
