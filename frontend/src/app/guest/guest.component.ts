3efac91ee68fb5182008a3f9a1d2ef38e42748d3
tatianazapata88 <taty0611z@gmail.com>
2021-05-19 13:08:19 -0500

    vista de acuerdo a rol

--- frontend/src/app/guest/guest.component.ts
+++ frontend/src/app/guest/guest.component.ts
@@ -6,6 +6,8 @@ import { isEmpty } from 'rxjs/operators';
 import Swal from 'sweetalert2';
 import { stringify } from '@angular/compiler/src/util';
 import { AuthService } from 'app/shared/services/auth.service';
+import { identifierModuleUrl } from '@angular/compiler';
+import { HttpResponse } from '@angular/common/http';
 
 
 declare var $: any
@@ -18,7 +20,7 @@ export class GuestComponent implements OnInit {
   username: any;
   password: any;
   showResult: boolean;
-  respuesta: Guest;
+  respuesta: any;
   rol:  any;
 
   constructor(private router: Router, private service: LoginService, private authService: AuthService) { }
@@ -46,8 +48,9 @@ export class GuestComponent implements OnInit {
     console.log(password);
 
     this.service.getLogin(username, password).subscribe(data => {
-      
-     console.log('este objeto llega data'+data)
+       
+     
+       console.log('esta es la resouesta login'+this.respuesta)
       if (data === null) {
         Swal.fire({
           icon: 'error',
@@ -57,24 +60,26 @@ export class GuestComponent implements OnInit {
         })
       }
       else {
-      
+        
         this.authService.login();
         this.routeRedirect = this.authService.urlUsuarioIntentAcceder;
         this.authService.urlUsuarioIntentAcceder = '';
         this.router.navigate([this.routeRedirect]);
-
-        console.log(data[0])
-        if(data[4]===2){
-          
-          this.respuesta = new Guest();
-          this.respuesta.roluser(username, password,rol)
-          this.router.navigate(['/admin/dashboard']);
-            console.log(this.respuesta.username)
+     
+        this.respuesta=data;
+        var usuario: Guest;
+        usuario=this.respuesta;
+        //console.log('posicion de data'+this.respuesta.rol)
+        //if(data[4]===2){
+         // console.log('este es lo que llega de back'+this.respuesta);
+         if(this.respuesta.rol==2){
+          this.router.navigate(['/admin/house']);
+            
 
         
-        console.log('el rol es'+this.respuesta.username)
+        console.log('el rol es'+this.respuesta.rol)
+         }
         
-        }
         else{
        
         this.router.navigate(['/admin/dashboard']);
--- frontend/src/app/sidebar/sidebar.component.ts
+++ frontend/src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Guest } from 'app/guest/guest';
+import { LoginService } from 'app/shared/services/login.service';
 
 declare const $: any;
 declare interface RouteInfo {
@@ -8,8 +10,9 @@ declare interface RouteInfo {
     class: string;
 }
 
-export const ROUTES: RouteInfo[] = [
-    { path: '/admin/dashboard', title: 'Buscar',  icon: 'pe-7s-home', class: '' },
+
+export const ROUTESGUEST: RouteInfo[] = [
+    { path: '/admin/dashboard', title: 'Bienvenido Portal GUEST',  icon: 'pe-7s-home', class: '' },
     { path: '/admin/user', title: 'Tu Perfil',  icon:'pe-7s-id', class: '' },
     { path: '/admin/table', title: 'Historial',  icon:'pe-7s-note2', class: '' },
     //{ path: '/admin/typography', title: 'Typography',  icon:'pe-7s-news-paper', class: '' },
@@ -17,8 +20,26 @@ export const ROUTES: RouteInfo[] = [
     //{ path: '/admin/maps', title: 'Maps',  icon:'pe-7s-map-marker', class: '' },
     //{ path: '/admin/notifications', title: 'Notifications',  icon:'pe-7s-bell', class: '' },
     //{ path: '/admin/calculadora', title: 'Calculadora',  icon:'pe-7s-calculator', class: '' },
-    { path: 'house', title: 'Quiero Ofrecer mi casa',  icon:'pe-7s-rocket', class: 'active-pro' },
+   // { path: 'house', title: 'Quiero Ofrecer mi casa',  icon:'pe-7s-rocket', class: 'active-pro' },
 ];
+
+export const ROUTESHOST: RouteInfo[] = [
+  { path: 'house', title: 'Bienvenido Portal HOST',  icon:'pe-7s-id', class: '' },
+  { path: '/admin/user', title: 'Tu Perfil',  icon:'pe-7s-id', class: '' },
+  { path: '/admin/table', title: 'Historial',  icon:'pe-7s-note2', class: '' },
+  //{ path: 'house', title: 'Quiero Ofrecer mi casa',  icon:'pe-7s-rocket', class: 'active-pro' },
+
+]
+
+export const ROUTES: RouteInfo[] = [
+  { path: '/admin/dashboard', title: 'Bienvenido Portal FULL',  icon: 'pe-7s-home', class: '' },
+  { path: '/admin/dashboard', title: 'Buscar',  icon: 'pe-7s-home', class: '' },
+  { path: '/admin/user', title: 'Tu Perfil',  icon:'pe-7s-id', class: '' },
+  { path: '/admin/table', title: 'Historial',  icon:'pe-7s-note2', class: '' },
+  { path: 'house', title: 'Quiero Ofrecer mi casa',  icon:'pe-7s-rocket', class: 'active-pro' },
+
+]
+
 
 @Component({
   selector: 'app-sidebar',
@@ -27,12 +48,28 @@ export const ROUTES: RouteInfo[] = [
 export class SidebarComponent implements OnInit {
   menuItems: any[];
 
-  constructor() { }
+  constructor(private service: LoginService) { }
 
   ngOnInit() {
-    
-    this.menuItems = ROUTES.filter(menuItem => menuItem);
-  }
+    let respuesta;
+    let username = this.service.username
+    let password = this.service.password;
+    this.service.getLogin(username, password).subscribe(data => {
+      respuesta=data;
+      var usuario: Guest;
+      usuario=respuesta;
+      console.log(usuario.rol)
+      if(usuario.rol==1){
+    this.menuItems = ROUTESGUEST.filter(menuItem => menuItem);
+      }
+      else if(usuario.rol==2){
+        this.menuItems = ROUTESHOST.filter(menuItem => menuItem);
+      }
+      else{
+        this.menuItems = ROUTES.filter(menuItem => menuItem);
+      }
+  })
+}
   isMobileMenu() {
       if ($(window).width() > 991) {
           return false;
